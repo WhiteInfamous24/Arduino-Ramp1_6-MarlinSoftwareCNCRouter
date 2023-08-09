16e657a617f89629d6e5d571aa02cee42a1c9037
WhiteInfamous24 <juanmanuel.caceres24@outlook.com>
2023-08-08 21:14:13 -0300

    Update Configuration.h

--- Marlin-2.1.2.1-custom-1.0/Marlin-2.1.2.1/Marlin/Configuration.h
+++ Marlin-2.1.2.1-custom-1.0/Marlin-2.1.2.1/Marlin/Configuration.h
@@ -1175,7 +1175,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 100, 100, 20 }
+#define DEFAULT_MAX_FEEDRATE          { 2100, 2100, 1000 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1188,7 +1188,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 100, 100, 10 }
+#define DEFAULT_MAX_ACCELERATION      { 100, 100, 20 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
